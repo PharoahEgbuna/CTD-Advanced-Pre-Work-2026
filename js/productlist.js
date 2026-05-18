a78c1c68d512515c9b2685dce737dc345bf98d40
Pharoah Egbuna <pharoahegbuna@gmail.com>
2026-05-17 20:39:52 -0400

    Refined comments and logic of productlist.js

--- js/productlist.js
+++ js/productlist.js
@@ -7,25 +7,53 @@ async function fetchProducts() {
     .then(response => response.json().then(data => data.pagination.total_pages));
 
     if (counter < total_pages) {
-        
-        //Fetch the current product page using the products API and the counter variable. 
-        let current_page = await fetch(`https://api.artic.edu/api/v1/products?page=${counter}&limit=12`)
-        .then(response => response.json());
+         
+        let current_page;
 
-        //Remove any pre-existing children of the product list to make room for new products.
+        //Attempt to fetch the a product page using the products API and the counter variable. If it cannot be reached, throw an error message and end function execution. 
+        try {
+            current_page = await fetch(`https://api.artic.edu/api/v1/products?page=${counter}&limit=12`)
+            
+            if (!current_page.ok) {
+                throw new Error("No data received");
+            }
+
+            current_page = await current_page.json();
+        } catch (error) {
+            console.error("Unable to fetch products:", error);
+            return;
+        }
+
+        //Remove any pre-existing children of the HTML product list to make room for new products.
         let productList = document.querySelector(".productlist");
         if (productList.hasChildNodes()) {
             productList.replaceChildren();
         }
 
+        //Populate the product list with each product of the current page. If a product field is not provided, use a placeholder value instead.
         for (let product of current_page.data) {
             let productItem = document.createElement("li");
             let productImage = document.createElement("img");
             let productTitle = document.createElement("h3");
             let productPrice = document.createElement("p");
 
-            productImage.src = product.image_url || 'images/no_picture_available.png';
-            productTitle.textContent = product.title || 'No Title Available';
+            let imageURL;
+            try {
+                imageURL = await fetch(product.image_url);
+                if (!imageURL.ok) {
+                    throw new Error("Image not found");
+                }
+            } catch (error) {
+                console.error(`Unable to fetch image for product ${product.id}:`, error);
+            }
+
+            if (!imageURL.ok) {
+                productImage.src = 'images/no_picture_available.png';
+            } else {
+                productImage.src = product.image_url;
+            }
+
+            productTitle.textContent = product.title ? product.title : 'No Title Available';
             productPrice.textContent = product.max_current_price ? `Price: $${product.max_current_price.toFixed(2)}` : 'No Price Available' ;
 
             //Append the product image, title, and price to the current list item.
@@ -39,15 +67,16 @@ async function fetchProducts() {
     }
 }
 
-
+//Increase the counter variable by 1 and call the fetchProducts function. 
 let nextPage = function () {
     counter++;
     fetchProducts();
     
-    //Display the 'Previous Page' button if the user is on the second page or higher.
+    //Display the 'Previous Page' button if the user is not on the first page.
     document.getElementById("previouspage").style.display = "inline-block";
 }
 
+//Decrease the counter variable by 1 and call the fetchProducts function.
 let previousPage = function () {
 
     if (counter >= 2) {
@@ -61,5 +90,5 @@ let previousPage = function () {
     }
 }
 
-//Call the function automatically to populate the page with products on page load.
+//Call the fetchProducts function on page load to populate with the first page of products.
 fetchProducts();
