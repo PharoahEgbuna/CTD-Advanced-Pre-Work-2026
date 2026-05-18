4df0ee9fba233699cc208dc7bf3e9f5238fb30cf
Pharoah Egbuna <pharoahegbuna@gmail.com>
2026-05-17 20:08:18 -0400

    Final touches to the artgenerator files. Added place of origin to the html and js files.

--- js/artgenerator.js
+++ js/artgenerator.js
@@ -4,19 +4,19 @@ function getRndInteger(min, max) {
 
 async function generateRandomArt() {
 
-    //Fetch the total number of pages from the artworks API endpoint.
+    //Fetch the total number of pages from the artworks endpoint.
     const totalPages =  await fetch("https://api.artic.edu/api/v1/artworks").then(response => response.json()).then(data => data.pagination.total_pages);
 
     //Generate a random page number between 1 and the total number of pages.
     let randomPageNum = getRndInteger(1, totalPages);
 
-    //Declare a variable to hold the HTTP response of the random page fetch request.
+    //Declare a the randomPage variable to hold the random page fetch request.
     let randomPage;
 
-    //Attempt to fetch a random page from the API endpoint. 
-    try {
-        //The URL is tailored to retrieve the following information from each artwork: id, title, artist_display, image_id, place_of_origin, and short_description.
-        //The page will have 12 artworks at most, as indicated by the limit parameter. Depending on the artwork, each field will have a null or non-null value.
+    //Attempt to fetch the random page. If the request fails, throw an error message in the console and end  function execution with an empty return statement.
+    try { 
+        //The URL is tailored to retrieve the following fields for each artwork: id, title, artist_display (artist), image_id, place_of_origin, and short_description.
+        //At most each page will have 12 artworks. Artwork fields may have null as their value.
         randomPage = await fetch(`https://api.artic.edu/api/v1/artworks?page=${randomPageNum}&limit=12&fields=id%2Ctitle%2Cartist_display%2Cimage_id%2Cplace_of_origin%2Cshort_description`)
         
         if (!randomPage.ok) {
@@ -24,16 +24,16 @@ async function generateRandomArt() {
         }
     } catch (error) {
         console.log(error.message);
+        return;
     }
 
-    //Convert the HTTP response to JSON and extract the data field, which is an array of up to 12 objects, each representing a different artwork.
+    //Convert the random page response to JSON and pull the data field, which is an array of up to 12 objects, each representing a different artwork.
     let pageData = await randomPage.json().then(data => data.data);
 
-    //Select a random artwork from the array using a random index between 0 and the length of the array minus 1.
-    let randomInt = getRndInteger(0, pageData.length - 1);
-    let randomArt = pageData[randomInt];
+    //Select a random artwork from the array and assign it to the randomArt variable. 
+    let randomArt = pageData[getRndInteger(0, pageData.length - 1)];
 
-    //Attempt to fetch the image file of the selected artwork, using the artwork's image_id field value.
+    //Attempt to fetch the image of the selected artwork. If the image cannot be retrieved, throw an error in the console and proceed. 
     let artworkURL;
     try {
         artworkURL =  await fetch(`https://www.artic.edu/iiif/2/${randomArt.image_id}/full/843,/0/default.jpg`);
@@ -45,18 +45,29 @@ async function generateRandomArt() {
         console.log(error.message);
     }
 
-    //Select the image, title, artist, description HTML elements by their id.
+    //Connect variables to the image, title, artist, origin, and description HTML elements.
     let image = document.getElementById("art-image"); 
     let title = document.getElementById("art-title");
     let artist = document.getElementById("artist");
+    let origin = document.getElementById("origin");
     let description = document.getElementById("description");
 
-    //Check if the artwork fields have valid, non-null values and assign them to the corresponding HTML elements. Otherwise, assign default values.
+    //Check if the randomArt properties have useable values and assign them to the HTML elements. If the values are not useable, then placeholder values are used instead. 
+    
+    //If the image file be reached, assign it to the image's src attribute. 
+    //Likewise, if the title was provided, assign that to the image's title attribute.
     image.src = artworkURL.ok ? `https://www.artic.edu/iiif/2/${randomArt.image_id}/full/843,/0/default.jpg` : "images/no_picture_available.png";
     image.title = randomArt.title ? randomArt.title : "No image available.";
 
+    //If the title was provided, assign it to the title paragraph element.
     title.textContent = randomArt.title ? `Title: "${randomArt.title}"` : "Title unknown.";
 
+    //If the artist was provided, assign it to the artist paragraph element.
     artist.textContent = randomArt.artist_display ? `Created by/in: ${randomArt.artist_display}.` : "Artist unknown.";
-    description.textContent = randomArt.short_description ? randomArt.short_description : "No description provided.";
+
+    //If the place of origin was provided, assign it to the origin paragraph element. 
+    origin.textContent = randomArt.place_of_origin ? `Place of origin: ${randomArt.place_of_origin}.` : "Place of origin unknown.";
+
+    //If the description was provided, assign it to the description paragraph element.
+    description.textContent = randomArt.short_description ? `Short description: ${randomArt.short_description}` : "No short description provided.";
 }
